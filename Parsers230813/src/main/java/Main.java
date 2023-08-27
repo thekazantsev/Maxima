31f9777af57612888549a7fc8fd9cb6aeb0ad12f
theKazantsev <djacidk@gmail.com>
2023-08-27 14:37:19 +1000

    git init Parsers230813

---
+++ Parsers230813/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Взять небольшой json и распарсить его 4 способами (Data bunding, Tree model, Streaming API, JsonPath)
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
