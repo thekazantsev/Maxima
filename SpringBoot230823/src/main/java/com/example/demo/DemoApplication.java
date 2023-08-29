7fd331a1629dd3d0aa298c93395d9b32ed611406
theKazantsev <djacidk@gmail.com>
2023-08-29 12:13:00 +1000

    init for SpringBoot230823

--- SpringBoot230823/src/main/java/com/example/demo/DemoApplication.java
+++ SpringBoot230823/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,13 @@
+package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
